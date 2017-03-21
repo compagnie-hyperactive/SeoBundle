01243dedc3d525f129cbac7b5780de41e5287b3b
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-03-21 16:19:32 +0100

    Minimal SEO output OK

--- Twig/SeoExtension.php
+++ Twig/SeoExtension.php
@@ -13,11 +13,24 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 
 class SeoExtension extends \Twig_Extension
 {
+    /**
+     * @var \Twig_Environment $twig
+     */
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig) {
+        $this->twig = $twig;
+    }
+
     public function getFunctions()
     {
         return array(
             new \Twig_SimpleFunction('getEntityClass', [$this, 'getEntityClass' ], [
                 'needs_environment' => false,
+            ]),
+            new \Twig_SimpleFunction('renderSeoTags', [$this, 'renderSeoTags' ], [
+                'needs_environment' => false,
+                'is_safe' => array('html')
             ]),
         );
     }
@@ -32,5 +45,14 @@ class SeoExtension extends \Twig_Extension
             throw new Exception();
         }
         return (new \ReflectionClass($entity))->getName();
+    }
+
+
+    public function renderSeoTags($entity = null) {
+        // TODO check entity is object and get Seoable trait
+        // TODO handle option for site title
+        return $this->twig->render('@LchSeo/Front/seo.html.twig', [
+            'entity' => $entity
+        ]);
     }
 }
