9a4caf3f3d99de08c76b9abfed792fddc6666720
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-03-05 11:47:41 +0100

    Introduce SlugType

---
+++ Form/SlugType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Lch\SeoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SlugType extends AbstractType
+{
+    /**
+     * The form name.
+     */
+    const NAME = 'lch_slug_type';
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('slug', TextType::class, array(
+                'translation_domain' => 'LchSeoBundle',
+                'required' => true,
+                'label' => 'lch.seo.form.url.name',
+                'block_name' => 'slug',
+                'attr' => [
+                    'helper' => 'lch.seo.form.description.helper',
+                ]
+            ))
+        ;
+    }
+
+    /**
+     * @inheritdoc
+     * inherit_data : https://symfony.com/doc/2.8/cookbook/form/inherit_data_option.html
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'inherit_data' => true,
+        ));
+    }
+    
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return self::NAME;
+    }
+}
