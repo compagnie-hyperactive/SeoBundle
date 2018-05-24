cad138f4ffc3cfdb83f0367793e751f9b3f3100b
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-05-24 16:06:26 +0200

    Add test on JS to avoid treatment if seoSulg button not found

--- Resources/public/js/seo.js
+++ Resources/public/js/seo.js
@@ -7,52 +7,54 @@ $(document).ready(function() {
     var $slugField =    $("#seo-slug input[name$='[slug]']");
     var $slugGenerateButton = $("#generateSlug");
 
-    var fields = JSON.parse($slugGenerateButton.attr('data-fields'));
+    if($slugGenerateButton.length === 1) {
+        var fields = JSON.parse($slugGenerateButton.attr('data-fields'));
 
-    /**
-     * Check if required field for slug is in DOM, to display slug 'generation' button
-     */
-    for(i in fields) {
-        if ($("input[name$='[" + fields[i] + "]']").length == 0) {
-            $slugGenerateButton.addClass("hidden");
-        }
-    }
-
-    
-    // On generate button click
-    $slugGenerateButton.click(function() {
-        // Check required fields for slug are set
-        var fieldsValues = {};
-
-        for(i in fields) {
-            if ($("input[name$='[" + fields[i] + "]']").val() == "") {
-                alert("Vous devez remplir tous les champs nécessaires pour générer le slug.");
-                return;
-            } else {
-                fieldsValues[fields[i]] = $("input[name$='[" + fields[i] + "]']").val();
+        /**
+         * Check if required field for slug is in DOM, to display slug 'generation' button
+         */
+        for (i in fields) {
+            if ($("input[name$='[" + fields[i] + "]']").length == 0) {
+                $slugGenerateButton.addClass("hidden");
             }
         }
 
-        $("#seo-slug .error-block").addClass('hidden');
 
-        var data = {};
-        data['fields'] = fieldsValues;
-        data['entityClass'] = $(this).attr('data-entity');
+        // On generate button click
+        $slugGenerateButton.click(function () {
+            // Check required fields for slug are set
+            var fieldsValues = {};
 
-        $.ajax({
-            url:  $slugGenerateButton.attr('data-route-generation-path'),
-            data : data,
-            method : 'POST',
-            success: function(result) {
-                if(result.success){
-                    $slugField.val(result.slug);
+            for (i in fields) {
+                if ($("input[name$='[" + fields[i] + "]']").val() == "") {
+                    alert("Vous devez remplir tous les champs nécessaires pour générer le slug.");
+                    return;
                 } else {
-                    $("#seo-slug .error-block span").html(result.message);
-                    $("#seo-slug .error-block").removeClass('hidden');
+                    fieldsValues[fields[i]] = $("input[name$='[" + fields[i] + "]']").val();
                 }
             }
+
+            $("#seo-slug .error-block").addClass('hidden');
+
+            var data = {};
+            data['fields'] = fieldsValues;
+            data['entityClass'] = $(this).attr('data-entity');
+
+            $.ajax({
+                url: $slugGenerateButton.attr('data-route-generation-path'),
+                data: data,
+                method: 'POST',
+                success: function (result) {
+                    if (result.success) {
+                        $slugField.val(result.slug);
+                    } else {
+                        $("#seo-slug .error-block span").html(result.message);
+                        $("#seo-slug .error-block").removeClass('hidden');
+                    }
+                }
+            });
         });
-    });
+    }
     //
     // ================================================================
 });
